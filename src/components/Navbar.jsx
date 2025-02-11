0026f17399c9790a20c2d1047b358b1e2378e6d3
Razaul007 <raselharuyamare@gmail.com>
2025-02-11 23:49:47 +0600

    Made Navbar Responsive with Dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,32 @@
 
+
 // import { Link, NavLink } from "react-router-dom";
+// import { useState, useEffect } from "react";
 // import useAuth from "../hooks/useAuth";
 
 
 // const Navbar = () => {
+    
+
 //     const { user, Logout } = useAuth();
-//     console.log(user)
+
+
+   
+   
 
 //     return (
 //         <div className="navbar bg-base-100 shadow-md sticky top-0 z-10 pb-5">
-//             {/* Navbar Start */}
 //             <div className="navbar-start">
-//                 {/* Logo */}
 //                 <Link to="/" className="text-xl lg:text-3xl font-bold text-slate-800">
 //                     EduSkills
 //                 </Link>
 //             </div>
-
-//             {/* Navbar Center */}
 //             <div className="navbar-center hidden lg:flex">
 //                 <ul className="menu menu-horizontal px-1">
-//                     {/* Common Links */}
+                   
 //                     <li><NavLink to="/" className="mr-4">Home</NavLink></li>
 //                     <li><NavLink to="/find-tutorials" className="mr-4">Find Tutors</NavLink></li>
-//                     {/* Links for Logged-in Users */}
+//                     {/*if user Logged-in  */}
 //                     {user && (
 //                         <>
 //                             <li><NavLink to="/add-tutorial" className="mr-4">Add Tutorials</NavLink></li>
@@ -34,16 +37,17 @@
 //                 </ul>
 //             </div>
 
-//             {/* Navbar End */}
 //             <div className="navbar-end flex items-center">
-//                 {/* If not logged in */}
+//                 {/* Theme Toggle */}
+                
+
+                
 //                 {!user && (
 //                     <>
-//                         {/* Login and Register Buttons */}
-//                         <Link className="mr-2 lg:mr-4 btn bg-cyan-500 text-xl lg:text-2xl" to="/login">
+//                         <Link className="mr-2 lg:mr-4 btn bg-cyan-800 text-xl text-white lg:text-2xl" to="/login">
 //                             Login
 //                         </Link>
-//                         <Link className="lg:mr-4 btn bg-cyan-500 text-xl lg:text-2xl" to="/register">
+//                         <Link className="lg:mr-4 btn bg-cyan-800 text-xl text-white lg:text-2xl" to="/register">
 //                             Register
 //                         </Link>
 //                     </>
@@ -51,14 +55,12 @@
 //                 {/* If logged in */}
 //                 {user && (
 //                     <div className="flex items-center space-x-4">
-//                         {/* Profile Picture */}
 //                         <img
 //                             src={user.photoURL || "/user.png"}
 //                             alt="Profile"
 //                             className="w-10 h-10 rounded-full"
 //                             title={user.displayName || "User"}
 //                         />
-//                         {/* Logout Button */}
 //                         <button
 //                             onClick={Logout}
 //                             className="btn bg-red-500 text-white"
@@ -76,59 +78,53 @@
 
 
 import { Link, NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useAuth from "../hooks/useAuth";
-
+import { FaBars, FaTimes } from "react-icons/fa"; // Importing icons for the menu
 
 const Navbar = () => {
-    
-
     const { user, Logout } = useAuth();
-
-
-   
-   
+    const [isOpen, setIsOpen] = useState(false); // State for the dropdown menu
 
     return (
-        <div className="navbar bg-base-100 shadow-md sticky top-0 z-10 pb-5">
-            <div className="navbar-start">
+        <div className="navbar bg-base-100 shadow-md sticky top-0 z-10 py-4 px-5 md:px-10  ">
+            <div className="navbar-start flex items-center justify-between w-full ">
                 <Link to="/" className="text-xl lg:text-3xl font-bold text-slate-800">
                     EduSkills
                 </Link>
+
+                {/* Mobile Menu Button */}
+                <button
+                    className="lg:hidden text-2xl"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {isOpen ? <FaTimes /> : <FaBars />}
+                </button>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                   
-                    <li><NavLink to="/" className="mr-4">Home</NavLink></li>
-                    <li><NavLink to="/find-tutorials" className="mr-4">Find Tutors</NavLink></li>
-                    {/*if user Logged-in  */}
+
+            {/* Desktop Menu */}
+            <div className="navbar-center hidden lg:flex ">
+                <ul className="menu menu-horizontal px-1 space-x-4">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/find-tutorials">Find Tutors</NavLink></li>
                     {user && (
                         <>
-                            <li><NavLink to="/add-tutorial" className="mr-4">Add Tutorials</NavLink></li>
-                            <li><NavLink to="/my-tutorials" className="mr-4">My Tutorials</NavLink></li>
-                            <li><NavLink to="/my-booked-tutors" className="mr-4">My booked tutors</NavLink></li>
+                            <li><NavLink to="/add-tutorial">Add Tutorials</NavLink></li>
+                            <li><NavLink to="/my-tutorials">My Tutorials</NavLink></li>
+                            <li><NavLink to="/my-booked-tutors">My Booked Tutors</NavLink></li>
                         </>
                     )}
                 </ul>
             </div>
 
-            <div className="navbar-end flex items-center">
-                {/* Theme Toggle */}
-                
-
-                
-                {!user && (
+            {/* Right Side (Login / Logout) */}
+            <div className="navbar-end hidden lg:flex items-center space-x-4">
+                {!user ? (
                     <>
-                        <Link className="mr-2 lg:mr-4 btn bg-cyan-800 text-xl text-white lg:text-2xl" to="/login">
-                            Login
-                        </Link>
-                        <Link className="lg:mr-4 btn bg-cyan-800 text-xl text-white lg:text-2xl" to="/register">
-                            Register
-                        </Link>
+                        <Link className="btn bg-cyan-800 text-white" to="/login">Login</Link>
+                        <Link className="btn bg-cyan-800 text-white" to="/register">Register</Link>
                     </>
-                )}
-                {/* If logged in */}
-                {user && (
+                ) : (
                     <div className="flex items-center space-x-4">
                         <img
                             src={user.photoURL || "/user.png"}
@@ -145,10 +141,46 @@ const Navbar = () => {
                     </div>
                 )}
             </div>
+
+            {/* Mobile Dropdown Menu */}
+            {isOpen && (
+                <div className="absolute top-16 left-0 w-full bg-white shadow-lg lg:hidden">
+                    <ul className="flex flex-col items-center space-y-4 py-4">
+                        <li><NavLink to="/" onClick={() => setIsOpen(false)}>Home</NavLink></li>
+                        <li><NavLink to="/find-tutorials" onClick={() => setIsOpen(false)}>Find Tutors</NavLink></li>
+                        {user && (
+                            <>
+                                <li><NavLink to="/add-tutorial" onClick={() => setIsOpen(false)}>Add Tutorials</NavLink></li>
+                                <li><NavLink to="/my-tutorials" onClick={() => setIsOpen(false)}>My Tutorials</NavLink></li>
+                                <li><NavLink to="/my-booked-tutors" onClick={() => setIsOpen(false)}>My Booked Tutors</NavLink></li>
+                            </>
+                        )}
+                        {!user ? (
+                            <>
+                                <li><Link className="btn bg-cyan-800 text-white" to="/login" onClick={() => setIsOpen(false)}>Login</Link></li>
+                                <li><Link className="btn bg-cyan-800 text-white" to="/register" onClick={() => setIsOpen(false)}>Register</Link></li>
+                            </>
+                        ) : (
+                            <li className="flex flex-col items-center">
+                                <img
+                                    src={user.photoURL || "/user.png"}
+                                    alt="Profile"
+                                    className="w-10 h-10 rounded-full"
+                                    title={user.displayName || "User"}
+                                />
+                                <button
+                                    onClick={() => { Logout(); setIsOpen(false); }}
+                                    className="btn bg-red-500 text-white mt-2"
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
 
 export default Navbar;
-
-
