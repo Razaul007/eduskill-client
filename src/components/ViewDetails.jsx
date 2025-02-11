1e9a5dcf5a7d280ef449d20e81b08d447db654d5
Razaul007 <raselharuyamare@gmail.com>
2025-02-11 21:28:23 +0600

    Updated ViewDetails page

--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -53,16 +53,16 @@ const ViewDetails = () => {
  
 
   return tutor ? (
-    <div className="w-1/2 mx-auto flex flex-col justify-center items-center gap-2 mt-10">
+    <div className="w-full md:w-1/2 mx-auto flex flex-col justify-center items-center gap-1 md:gap-2 mt-2 md:mt-10">
     
-      <img className=" rounded-lg p-2" src={tutor.image} alt={tutor.name} />
+      <img className=" rounded-xl p-2" src={tutor.image} alt={tutor.name} />
       <h1>{tutor.name}</h1>
       <p>Language: {tutor.language}</p>
-      <p className="w-1/2">Description: {tutor.description}</p>
-      <p>Price: {tutor.price}</p>
+      <p className="w-full p-2 md:w-1/2 ">Description: {tutor.description}</p>
+      <p>Price: {tutor.price}$</p>
       <p>Review: {tutor.review}</p>
       {user ? (
-        <button className="btn btn-outline w-1/4 bg-slate-400 text-white " onClick={handleBooking}>Book</button>
+        <button className="btn btn-outline w-1/4 bg-cyan-800 text-white " onClick={handleBooking}>Book</button>
       ) : (
         <p>Please log in to book a tutor.</p>
       )}
